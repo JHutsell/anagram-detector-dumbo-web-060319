079f9e98855358eaa3ea9b161b43cfaedfe42976
Justin Hutsell <hutselljustin@gmail.com>
2019-06-09 19:26:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,8 @@ class Anagram
   end
   
   def match(arr)
-    sorted_word = @word.split("").sort.join("") 
+    sorted_word = @word.split("").sort.join("")
+    #seems overly complicated but only way to sort a string simply is to split it first
     arr.select { |i| sorted_word == i.split("").sort.join("") } 
   end
   
